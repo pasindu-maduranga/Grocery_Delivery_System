f47a85aa5609153be09a8ff3ec50c287d963651e
sewmi <sewmisenarath@gmail.com>
2026-03-02 12:37:26 +0530

    google sign up completed

--- User_Management_and_Payment_Service/backend/index.js
+++ User_Management_and_Payment_Service/backend/index.js
@@ -1,16 +1,21 @@
+require('dotenv').config();
 const path = require('path');
 const express = require('express');
-const dotenv = require('dotenv');
 const connectDB = require('./src/config/db');
 const routes = require('./src/routes/indexRoutes');
+const sessionSetup = require('./src/services/sessionSetup');
+const passport = require('./src/utils/passportUtils');
 
-dotenv.config();
 connectDB();
 
 const app = express();
 
 app.use(express.json());
 app.use(express.static(path.join(__dirname, 'public')));
+sessionSetup(app);
+
+app.use(passport.initialize());
+app.use(passport.session());
 
 routes(app);
 
