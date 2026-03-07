7aacf77d040ffe0f73c2c9fa037e1c9e4d6bface
sewmi <sewmisenarath@gmail.com>
2026-03-08 00:49:39 +0530

    UI for customer login,register,forgot password, reset password with API integration done

--- User_Management_and_Payment_Service/backend/src/utils/passportUtils.js
+++ User_Management_and_Payment_Service/backend/src/utils/passportUtils.js
@@ -1,7 +1,7 @@
 const passport = require('passport');
 const googleStrategy = require('passport-google-oauth20').Strategy;
 const User = require('../models/UserModel');
-const { generateAccessToken } = require('./generateToken');
+const generateAccessToken = require('./generateToken');
 
 passport.use(
     new googleStrategy(
@@ -13,18 +13,28 @@ passport.use(
         async(accessToken, refreshToken, profile, done) => {
             try{
                 //find an existing user by email
-                let user = await User.findOne({ email: profile.emails[0].value });
+                let user = await User.findOne({ 
+                    $or: [
+                        { googleId: profile.id },
+                        { email: profile.emails[0].value }
+                    ]
+                });
 
                 //if user is not found, create a new user
                 if(!user){
                     user = new User({
+                        googleId: profile.id,    
                         name: profile.displayName,
                         email: profile.emails[0].value,
-                        avatar: profile.photos[0].value,  // save Google profile pic
+                        avatar: profile.photos?.[0]?.value || '',
                         role: 'customer',
                         isVerified: true,  //auto-verify after Google confirmed
-                        password: undefined,  //no password for OAuth users
+                        // password: undefined,  //no password for OAuth users
                     });
+                    await user.save();
+                }else if (!user.googleId) {
+                    //link googleId to existing email account
+                    user.googleId = profile.id;
                     await user.save();
                 }
 
