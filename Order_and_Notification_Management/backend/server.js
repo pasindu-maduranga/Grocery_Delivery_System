c5b19e5b9c9443dac5204a16dc8b3eacbb2a77d0
Lashan20689 <it22194176@my.sliit.lk>
2026-03-08 10:22:52 +0530

    Setup Backend for order management System

---
+++ Order_and_Notification_Management/backend/server.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const dotenv = require('dotenv');
+const connectDB = require('./src/config/db'); // ✅ ADD THIS
+
+dotenv.config();
+
+const app = express();
+
+// ✅ CONNECT DATABASE
+connectDB();
+
+// Middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Routes
+// const deliveryRoutes = require('./src/routes/delivery.routes');
+// app.use('/api/delivery', deliveryRoutes);
+
+// Test Route
+app.get('/', (req, res) => {
+  res.send('Delivery Service Running');
+});
+
+// Start Server
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Delivery Service running on port ${PORT}`);
+});
